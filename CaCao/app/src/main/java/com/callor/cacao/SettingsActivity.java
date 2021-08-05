398fc8cf02bbb53b97e2da3d2d4ca0ae7fe10ebc
ioo1332 <ioo168400@gmail.com>
2021-08-05 18:14:20 +0900

    chatt

---
+++ CaCao/app/src/main/java/com/callor/cacao/SettingsActivity.java
@@ -0,0 +1,59 @@
+package com.callor.cacao;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int setting_item=item.getItemId();
+        /**
+         * MainActivity 에서 호출되어 열린 새로운 Activity에 있는
+         * optionMenu 의 item 은
+         * "R.id.아이템" 으로 사용하지 않고
+         * "android.R.id.아이템" 으로 사용 해야 한다
+         */
+        if(setting_item==android.R.id.home){
+            /**
+             * onBackPressed() method 는 이전 화면으로 이동하는 method
+             * 현재 Activity 를 닫고 이전 Activity 를 열어주는 method
+             * 이 method 가 이전 Activity 를 정확히 인지하도록 Manifest.xml 의
+             * 현재 Activity 설정 부분에 다음의 코드를 추가 해주자
+             * android:parentActivityName=".MainActivity"
+             */
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+}
--- CaCao/app/src/main/java/com/callor/cacao/service/FirebaseServiceImplV1.java
+++ CaCao/app/src/main/java/com/callor/cacao/service/FirebaseServiceImplV1.java
@@ -8,15 +8,30 @@ import com.callor.cacao.model.Chatt;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-
+/**
+ * ChildEventListener
+ * Firebase 의 RealtimeDataBase 에 변화 (CUD)가 발생하면
+ * Firebase 에서 Android 모든 App 에 신호를 보내고
+ * 그 신호를 event 로 받아서 처리하는 클래슬르 작성하기 위한 interface
+ */
 public class FirebaseServiceImplV1 implements ChildEventListener {
-
+    /**
+     * ChattAdapter 객체가 하는일
+     * RecyclerView 와 연동하여 데이터를 화며에 그리기 위한 중간 연결도구
+     * Adapter 를 MainActivity 로 부터 전달받아
+     * FireBase Database 에 데이터가 추가되면 데이터를 가져와서 화면에 그리기 위한 코드를 작성한다
+     */
     private ChattAdapter adapter;
 
     public FirebaseServiceImplV1(ChattAdapter adapter) {
         this.adapter = adapter;
     }
-
+    /**
+    *   CUD 중에서 새로운 데이터가 추가되는 (insert,create) event 가 발생하면 실행되는 method
+     *   이 method 에서 데이터가 추가되었다는 신호를 받으면 데이터를 가져와서
+     *   Adapter 의 chattList 에 데이터를 추가한다
+     *   그리고 RecyclerView 에게 알림을 전한다
+    */
     @Override
     public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
