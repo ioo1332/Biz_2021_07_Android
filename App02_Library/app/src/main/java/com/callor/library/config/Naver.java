a482a3ac66b5e28ff22cff57e2dda8440bdf716d
ioo1332 <ioo168400@gmail.com>
2021-08-06 10:50:01 +0900

    library

---
+++ App02_Library/app/src/main/java/com/callor/library/config/Naver.java
@@ -0,0 +1,8 @@
+package com.callor.library.config;
+
+public class Naver {
+
+    public static String CLIENT_ID="YOUR NAVER CLIETNT ID";
+    public static String CLIENT_SECRET="YOUR NAVER CLIETNT SECRET";
+
+}
