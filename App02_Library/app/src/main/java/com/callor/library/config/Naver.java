c26b3b0eee0683cbfa92487f44624b1b73f4771a
ioo1332 <ioo168400@gmail.com>
2021-08-06 11:05:47 +0900

    library

--- App02_Library/app/src/main/java/com/callor/library/config/Naver.java
+++ App02_Library/app/src/main/java/com/callor/library/config/Naver.java
@@ -1,7 +1,7 @@
 package com.callor.library.config;
 
 public class Naver {
-
+    // Naver Open API 연동을 위한 id와 비밀번호
     public static String CLIENT_ID="YOUR NAVER CLIETNT ID";
     public static String CLIENT_SECRET="YOUR NAVER CLIETNT SECRET";
 
