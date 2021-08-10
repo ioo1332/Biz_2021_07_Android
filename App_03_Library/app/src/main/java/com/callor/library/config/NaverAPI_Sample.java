d054a2b484410ce3174c297d13f3e8c93cda3219
ioo1332 <ioo168400@gmail.com>
2021-08-10 09:47:24 +0900

    API

---
+++ App_03_Library/app/src/main/java/com/callor/library/config/NaverAPI_Sample.java
@@ -0,0 +1,14 @@
+package com.callor.library.config;
+
+/**
+ * Naver OpenAPI 에 접근하기 위한 정보들을 포함하는 클래스
+ * Naver OpenAPI 관련 정보를 변경한후
+ * NaverAPI 로 변경하여 프로젝트를 빌드
+ */
+public class NaverAPI_Sample {
+
+    public static final String CLIENT_ID="YOUR NAVER API ID";
+    public static final String CLIENT_SECRET="YOUR NAVER API SECRET";
+    public static final String CLIENT_BOOK_URL="https://openapi.naver.com/v1/search/book.json";
+    public static final String CLIENT_BASE_URL="https://openapi.naver.com/v1/search/";
+}
